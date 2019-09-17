b8ce7033bef406be400ca6ac64595ef4388657cb
beyond <921790476>
2019-09-17 10:57:43 +0800

    自定义异常

--- xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
@@ -1,18 +1,63 @@
 package com.xuecheng.framework.exception;
 
+import com.google.common.collect.ImmutableMap;
+import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.framework.model.response.ResultCode;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class ExceptionCatch {
+
+    /**
+     * 定义线程安全 map进行存储预置得错误和编码
+     */
+    private static ImmutableMap<Class<? extends Throwable>, ResultCode> EXCEPTIONS;
+
+    /**
+     * 构建map数据
+     */
+    private static ImmutableMap.Builder<Class<? extends Throwable>, ResultCode> builder = ImmutableMap.builder();
+
+    /**
+     * 捕获自定义异常
+     *
+     * @param o 自定义异常
+     * @return ResponseResult
+     */
     @ExceptionHandler(CustomException.class)
     @ResponseBody
     public ResponseResult customException(CustomException o) {
         ResultCode resultCode = o.getResultCode();
-        ResponseResult responseResult = new ResponseResult(resultCode);
+        return new ResponseResult(resultCode);
+    }
+
+    /**
+     * 捕获系统异常
+     *
+     * @param o 系统异常
+     * @return ResponseResult
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseResult exception(Exception o) {
+        if (EXCEPTIONS == null) {
+            EXCEPTIONS = builder.build();
+        }
+        final ResultCode resultCode = EXCEPTIONS.get(o.getClass());
+        final ResponseResult responseResult;
+        if (resultCode != null) {
+            responseResult = new ResponseResult(resultCode);
+        } else {
+            responseResult = new ResponseResult(CommonCode.SERVER_ERROR);
+        }
         return responseResult;
+    }
+
+    static {
+        builder.put(HttpMessageNotReadableException.class, CommonCode.INVALIDPARAM);
     }
 }
--- xc-framework-common/src/main/java/com/xuecheng/framework/model/response/CommonCode.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/model/response/CommonCode.java
@@ -10,21 +10,22 @@ import lombok.ToString;
  */
 
 @ToString
-public enum CommonCode implements ResultCode{
+public enum CommonCode implements ResultCode {
 
-    SUCCESS(true,10000,"操作成功！"),
-    FAIL(false,11111,"操作失败！"),
-    UNAUTHENTICATED(false,10001,"此操作需要登陆系统！"),
-    UNAUTHORISE(false,10002,"权限不足，无权操作！"),
-    SERVER_ERROR(false,99999,"抱歉，系统繁忙，请稍后重试！");
-//    private static ImmutableMap<Integer, CommonCode> codes ;
+    SUCCESS(true, 10000, "操作成功！"),
+    FAIL(false, 11111, "操作失败！"),
+    UNAUTHENTICATED(false, 10001, "此操作需要登陆系统！"),
+    UNAUTHORISE(false, 10002, "权限不足，无权操作！"),
+    INVALIDPARAM(false, 10003, "参数非法！"),
+    SERVER_ERROR(false, 99999, "抱歉，系统繁忙，请稍后重试！");
     //操作是否成功
     boolean success;
     //操作代码
     int code;
     //提示信息
     String message;
-    private CommonCode(boolean success,int code, String message){
+
+    CommonCode(boolean success, int code, String message) {
         this.success = success;
         this.code = code;
         this.message = message;
@@ -34,6 +35,7 @@ public enum CommonCode implements ResultCode{
     public boolean success() {
         return success;
     }
+
     @Override
     public int code() {
         return code;
