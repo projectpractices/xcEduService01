067cf3ae1e18cc56b62edfb711ca767ed2183551
beyond <921790476>
2019-09-23 17:13:13 +0800

    修改一处文本错误

--- xc‐service‐manage‐cms‐client/src/main/java/com/xuecheng/manage/cms/client/config/RabbitmqConfig.java
+++ xc‐service‐manage‐cms‐client/src/main/java/com/xuecheng/manage/cms/client/config/RabbitmqConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitmqConfig {
 
     //队列名称
-    public static final String QUEUE_CMS_POSTPAGE = "queue_cms_postpage";
+    public static final String QUEUE_CMS_POSTPAGE = "queue_cms_postpage_01";
     //交换机名称
     public static final String EX_ROUTING_CMS_POSTPAGE = "ex_routing_cms_postpage";
 
